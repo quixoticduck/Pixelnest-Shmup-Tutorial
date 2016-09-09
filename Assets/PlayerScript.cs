ed22a8cffb755c5b3c4e91d2413ab2f4a2f30722
Clara Alder <quixoticduck@gmail.com>
2016-09-09 01:41:34 +0100

    first commit

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Player controller and behavior
+/// </summary>
+public class PlayerScript : MonoBehaviour
+{
+	/// <summary>
+	/// 1 - The speed of the ship
+	/// </summary>
+	public Vector2 speed = new Vector2(50, 50);
+	
+	// 2 - Store the movement
+	private Vector2 movement;
+	
+	void Update()
+	{
+		// 3 - Retrieve axis information
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		
+		// 4 - Movement per direction
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY);
+
+        //Using the weapon with the player entity
+        //Let’s go back to “PlayerScript”.In the Update() method, put this snippet:
+        // 5 - Shooting
+        bool shoot = Input.GetButtonDown("Fire1");
+        shoot |= Input.GetButtonDown("Fire2");
+        // Careful: For Mac users, ctrl + arrow is a bad idea
+
+        if (shoot)
+        {
+            WeaponScript weapon = GetComponent<WeaponScript>();
+            if (weapon != null)
+            {
+                // false because the player is not an enemy
+                weapon.Attack(false);
+            }
+        }
+    }
+
+    void FixedUpdate()
+	{
+		// 5 - Move the game object
+		GetComponent<Rigidbody2D>().velocity = movement;
+	}
+}
